cadb6022a07f974768599698c4c11deb9818abc2
sadiyaaysha <32979416+sadiyaaysha@users.noreply.github.com>
2018-02-07 19:52:28 +0530

    Create rr.c

---
+++ rr.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	int a,b,c;
+	scanf("%d%d%d",&a,&b,&c);
+	if(a>b && a>c) 
+	
+		printf("a is great");
+	
+		else if(b>c)
+		
+			printf("bis great");
+		
+		else
+		printf("c is greta");
+	
+	return 0;
+}
