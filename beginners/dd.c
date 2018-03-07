d72e684462ea58b5f8097c531c786bb4de265576
sadiyaaysha <32979416+sadiyaaysha@users.noreply.github.com>
2018-03-07 12:48:28 +0530

    Create dd.c

---
+++ beginners/dd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	char a;
+	scanf("%c",&a);
+	if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U')
+	{
+		printf("vowel");
+	}
+	else
+	printf("consonant");
+	return 0;
+}
