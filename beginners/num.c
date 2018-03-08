df87943d694fff9006643fd6aea6451fda770d0b
sadiyaaysha <32979416+sadiyaaysha@users.noreply.github.com>
2018-03-08 19:25:33 +0530

    Create num.c

---
+++ beginners/num.c
@@ -0,0 +1,29 @@
+ 
+
+    #include <stdio.h>
+
+    void main()
+
+    {
+
+ 
+
+        int i, j, a, n, number[30];
+
+        printf("Enter the value of N \n");
+
+        scanf("%d", &n);
+
+ 
+
+       if(n>=0&&n<=9)
+       {
+       	printf("yes");
+       }
+
+
+ 
+else
+printf("no");
+return 0;
+    }
