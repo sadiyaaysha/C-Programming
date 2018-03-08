dfb86845574aa7979e754fdfca1cd2d6aebf0787
sadiyaaysha <32979416+sadiyaaysha@users.noreply.github.com>
2018-03-08 19:51:40 +0530

    Create check.c

---
+++ beginners/check.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+
+int main(void)
+{
+int n,k;
+scanf("%d",&n);
+scanf("%d",&k);
+int a[n];
+int i;
+
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+for(i=0;i<n;i++)
+{
+	if(a[i]==k)
+	printf("yes");
+}
+return 0;
+}
