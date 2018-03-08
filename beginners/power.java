39a5c079a24a4e5dfe80278aa9b317954bc57aec
sadiyaaysha <32979416+sadiyaaysha@users.noreply.github.com>
2018-03-08 17:49:54 +0530

    Create power.java

---
+++ beginners/power.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n;
+		Scanner scan=new Scanner(System.in);
+		n=scan.nextInt();
+		int k;
+		k=scan.nextInt();
+		System.out.print((int)Math.pow(n,k));
+	
+	}
+}
