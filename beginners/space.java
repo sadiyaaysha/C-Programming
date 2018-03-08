165bfde954699ab8b320fd174c7f21a6d888db50
sadiyaaysha <32979416+sadiyaaysha@users.noreply.github.com>
2018-03-08 22:26:07 +0530

    Create space.java

---
+++ beginners/space.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	String d;
+	Scanner scan=new Scanner(System.in);
+	d=scan.nextLine();
+	String[] f=d.split("\t");
+	System.out.print(f.length);
+	}
+}
