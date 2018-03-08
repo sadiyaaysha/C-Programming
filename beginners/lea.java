93033412f768679f70b510446c0eeb9b26c4f659
sadiyaaysha <32979416+sadiyaaysha@users.noreply.github.com>
2018-03-08 17:45:45 +0530

    Create lea.java

---
+++ beginners/lea.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n;
+		Scanner scan=new Scanner(System.in);
+		n=scan.nextInt();
+		if(n%4==0)
+		{
+			System.out.print("leap");
+			
+		}
+		else
+	System.out.printf("not");
+	}
+}
