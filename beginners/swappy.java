b1f75407e9133110ef7d4a3f9050e0fae43e2831
sadiyaaysha <32979416+sadiyaaysha@users.noreply.github.com>
2018-03-08 22:39:05 +0530

    Create swappy.java

---
+++ beginners/swappy.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int a=5;
+	int b=8;
+	int c;
+	c=a;
+	a=b;
+	b=c;
+	System.out.print(a,b);
+	}
+}
