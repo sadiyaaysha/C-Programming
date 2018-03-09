68af3286ed4f796e9f71f35db121e0b4494d7581
sadiyaaysha <32979416+sadiyaaysha@users.noreply.github.com>
2018-03-09 09:30:36 +0530

    Create ew.java

---
+++ beginners/ew.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+String s;
+int k;
+Scanner scan=new Scanner(System.in);
+s=scan.nextLine();
+k=scan.nextInt();
+int i=0;
+while(i<k)
+{
+	System.out.println(s);
+	i++;
+}
+	}
+}
