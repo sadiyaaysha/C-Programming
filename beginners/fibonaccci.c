0c36852fa12b8dcc595e2ab0c4fbf07b7a54677d
sadiyaaysha <32979416+sadiyaaysha@users.noreply.github.com>
2018-03-10 15:50:28 +0530

    Create fibonaccci.c

---
+++ beginners/fibonaccci.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<string.h>
+int main(void) {
+	int n;
+	scanf("%d",&n);
+int	n1=0;
+int	n2=1;
+	int n3;
+	while(n3<=n)
+	{
+	printf("%d ",n3);
+	n3=n1+n2;
+	n1=n2;
+	n2=n3;
+
+	}
+
+return 0;	
+}
