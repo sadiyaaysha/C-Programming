28fd62e160c6972b5d424f7fa2aa57ca140f977f
sadiyaaysha <32979416+sadiyaaysha@users.noreply.github.com>
2018-03-10 18:14:28 +0530

    Create 50.c

---
+++ guvi practice/50.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+#include<math.h>
+int main(void) {
+int n=110/100;
+int h=99%10;
+int y=2147483647/2147483647;
+int r=2147483646%2147483647;
+printf("%d %d %d %d",n,h,y,r);
+}
