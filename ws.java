21140b7b53e40b7fb45e167c144b372d5ccaa498
sadiyaaysha <32979416+sadiyaaysha@users.noreply.github.com>
2018-03-10 21:00:46 +0530

    Create ws.java

---
+++ ws.java
@@ -0,0 +1,30 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+String d;
+
+Scanner scan=new Scanner(System.in);
+d=scan.next();
+if((d.equals("saturday")))
+{
+System.out.print("yes");
+}
+else if(d.equals("sunday"))
+{
+	System.out.print("yes");
+}
+else
+{
+System.out.print("no");
+}
+
+	}
+}
