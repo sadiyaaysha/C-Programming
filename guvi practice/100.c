84a8360db44f57f8f6e70e262dad2faa4736ece8
sadiyaaysha <32979416+sadiyaaysha@users.noreply.github.com>
2018-03-10 17:51:02 +0530

    Create 100.c

---
+++ guvi practice/100.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include<math.h>
+int main(void) {
+int n;
+int n1;
+int k;
+int k1;
+int g;
+int g1;
+scanf("%d%d%d%d%d%d",&n,&n1,&k,&k1,&g,&g1);
+int f=abs(n-n1);
+int f1=abs(k-k1);
+int f2=abs(g-g1);
+printf("%d%d%d",f,f1,f2);
+}
